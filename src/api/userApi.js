a00db0a161c6df2e1dcd4d3d02d0087c3a7d3b73
Jaakko Tulkki <jaakko.tulkki@sainsburys.co.uk>
2016-12-01 09:24:59 +0000

    done

--- src/api/userApi.js
+++ src/api/userApi.js
@@ -1,11 +1,24 @@
 import 'whatwg-fetch';
+import getBaseUrl from './baseUri';
 
 export function getUsers(){
   return get('users');
 }
 
+export function deleteUser(id) {
+  return del(`users/${id}`);
+}
+
 function get(url) {
-  return fetch(url).then(onSuccess, onError);
+  return fetch(getBaseUrl() + url).then(onSuccess, onError);
+}
+
+function del(url) {
+  const request = new Request(getBaseUrl() + url, {
+    method: 'DELETE'
+  });
+
+  return fetch(request).then(onSuccess, onError);
 }
 
 function onSuccess(response) {
